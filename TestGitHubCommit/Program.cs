9639790b3f15d139b8141eba37151e42ef888170
PhilipGuo <Philip.guo1113@gmail.com>
2017-04-06 12:42:58 +0800

    test commit github
    this is an application to test commit github

---
+++ TestGitHubCommit/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestGitHubCommit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("this is an application to test commition of GitHub.");
+            Console.Read();
+        }
+    }
+}
